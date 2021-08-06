a572f8a7e9f13e9b8a86ebeade3b0e82a4928dc0
D3XT3R- <mrneth58@gmail.com>
2021-08-06 13:42:06 +0700

    UI Addition

---
+++ Assets/Scripts/GroundController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundController : MonoBehaviour
+{
+    public GameObject GameOver;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Block")
+        {
+            GameOver.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
